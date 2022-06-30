fe433dba9de15c3d52a0671fc4309eed12cd3e41
MikhailPtichkin88 <mikhailptichkin1988@gmail.com>
2022-06-30 14:56:02 +0300

    Lesson 56: Container component Users refactoring

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,56 +1,45 @@
 import React from 'react';
-import c from './Users.module.scss'
+import c from "./Users.module.scss";
+import ava from "../../images/avatars/Sveta.svg";
 import User from "./User/User";
-import {UsersPropsType} from "./UsersContainer";
-import axios from "axios";
-import ava from '../../images/avatars/Sveta.svg'
 import {UserType} from "../../redux/users-reducer";
 
+type UsersPresentationPropsType ={
+    totalUsersCount:number
+    pageSize:number
+    currentPage:number
+    onClickHandler:(p:number)=>void
+    onChangeHandler:(userId: string, isFollowed: boolean) =>void
+    users:Array<UserType>
+}
 
-class Users extends React.Component<UsersPropsType, any> {   //типизация <пропсов, стейта>
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-            this.props.setTotalUsersCount(response.data.totalCount)
-        })
+const Users = (props:UsersPresentationPropsType) => {
+
+    let pagesCount = Math.ceil( props.totalUsersCount / props.pageSize)
+    let pages: Array<number> = []
+
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
     }
 
-    onClickHandler = (p: number) => {
-
-        this.props.onChangePageHandler(p)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${p}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-            console.log(response.data.items)
-        })
-    }
-
-    render() {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
-        let pages: Array<number> = []
-
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i)
-        }
-
-        let curP = this.props.currentPage;
-        let curPF = ((curP - 5) < 0) ? 0 : curP - 5;
-        let curPL = curP + 5;
-        let slicedPages = pages.slice(curPF, curPL);
-        console.log(slicedPages)
+    let curP = props.currentPage;
+    let curPF = ((curP - 3) < 0) ? 0 : curP - 3;
+    let curPL = ((curP) > 3) ? curP+3 : curP + 2;
+    let slicedPages = pages.slice(curPF, curPL);
 
 
-        return <div className={c.wrapper}>
+    return (
+        <div className={c.wrapper}>
 
             <ul className={c.pagination}>
                 {
                     slicedPages.map(p => {
 
-                        let pageClasses = (this.props.currentPage === p) ? c.paginationLink + ' ' + c.activePage : c.paginationLink
+                        let pageClasses =  props.currentPage === (p) ? c.paginationLink + ' ' + c.activePage : c.paginationLink
 
                         return <li key={p} className={c.paginationItem}>
-                            <button onClick={() => this.onClickHandler(p)} className={pageClasses}>{p}
+                            <button onClick={() => props.onClickHandler(p)} className={pageClasses}>{p}
                             </button>
                         </li>
                     })
@@ -59,9 +48,9 @@ class Users extends React.Component<UsersPropsType, any> {   //типизаци�
             </ul>
             <ul className={c.users_list}>
                 {
-                    this.props.users.map(u => {
+                    props.users.map(u => {
                         const onChangeHandler = (isFollowed: boolean) => {
-                            this.props.onChangeHandler(u.id, isFollowed)
+                            props.onChangeHandler(u.id, isFollowed)
                         }
                         const photo = (u.photos.small || u.photos.large) ? (u.photos.small || u.photos.large) : ava
 
@@ -74,7 +63,7 @@ class Users extends React.Component<UsersPropsType, any> {   //типизаци�
             </ul>
             <button className={c.show_more_btn}>Show more</button>
         </div>
-    }
-}
+    );
+};
 
 export default Users;
--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Users from "./Users";
 import {connect} from "react-redux";
 import {Dispatch} from "redux";
 import {
@@ -10,11 +9,10 @@ import {
     UsersActionType,
     UserType
 } from "../../redux/users-reducer";
-import {v1} from "uuid";
 import {ReduxStoreType} from "../../redux/redux-store";
 
-
-
+import axios from "axios";
+import Users from "./Users";
 
 
 type MapStateToPropsType = {
@@ -59,5 +57,39 @@ let mapDispatchToState = (dispatch: Dispatch<UsersActionType>):MapDispatchToProp
     }
 }
 
-const UsersContainer = connect(mapPropsToState, mapDispatchToState)(Users)
+ class UsersAPI extends React.Component<UsersPropsType, ReduxStoreType> {   //типизация <пропсов, стейта>
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
+        })
+    }
+
+    onClickHandler = (p: number) => {
+
+        this.props.onChangePageHandler(p)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${p}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+        })
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      onClickHandler={this.onClickHandler}
+                      onChangeHandler={this.props.onChangeHandler}
+                      users={this.props.users}
+        />
+    }
+}
+
+
+
+
+
+
+
+const UsersContainer = connect(mapPropsToState, mapDispatchToState)(UsersAPI)
 export default UsersContainer
